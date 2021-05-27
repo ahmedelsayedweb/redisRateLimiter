ce322d027e14a613ed88b6522551fbe04b56e687
Ahmed Elsayed <ahmed.elsayed@mideastsoft.com>
2021-05-27 10:01:15 +0200

    finish

--- middleware/ratelimiter.js
+++ middleware/ratelimiter.js
@@ -1,18 +1,19 @@
-const redis = require('redis')
-const redisClient = redis.createClient()
+const redis = require('redis');
+const redisClient = redis.createClient();
 const requestIp = require('request-ip');
 
 redisClient.on("ready", function () {
     console.log("Redis connection ready ");
 });
 const rateLimiter = (req, res, next) => {
-    redisClient.get('connect' + requestIp.getClientIp(req), (err, reply) => {
+    const ip = requestIp.getClientIp(req)
+    redisClient.get('connect' + ip, (err, reply) => {
         if (reply >= 3) {
-            return res.status(429).json({ "error": 1, "message": "Too Many Requests..." })
+            return res.status(429).json({ "error": 1, "message": "Too Many Requests..." });
         } else if (reply > 0) {
-            redisClient.incrby('connect' + requestIp.getClientIp(req), 1);
+            redisClient.incrby('connect' + ip, 1);
         } else {
-            redisClient.set('connect' + requestIp.getClientIp(req), 1, 'EX', 60);
+            redisClient.set('connect' + ip, 1, 'EX', 60);
         }
         next()
     })
